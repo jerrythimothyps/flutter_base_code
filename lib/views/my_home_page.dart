067b196eec2f8a05d5c5e7cffcc649c018270f66
Jerry Thimothy <jerry.thimothy@publicissapient.com>
2021-03-03 10:49:48 +0400

    Getx is nice

---
+++ lib/views/my_home_page.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import '../controllers/my_home_page_controller.dart';
+
+class MyHomePage extends StatelessWidget {
+  final String title;
+  final MyHomePageController controller = Get.put(MyHomePageController());
+  MyHomePage({this.title});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times1:',
+            ),
+            Obx(
+              () => Text(
+                '${controller.count.value}',
+                style: Theme.of(context).textTheme.headline4,
+              ),
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: controller.increment,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
